4a89afb8c7ac356f6c95f6f4621029d1cdac55fb
ragnraok <okone1288@gmail.com>
2015-10-02 23:16:09 +0800

    add RemoveLinkedListElements

---
+++ RemoveLinkedListElements.py
@@ -0,0 +1,15 @@
+def removeElements(head, val):
+    if head is None:
+        return head
+    ret = head
+    cur = head.next
+    while cur.next is not None:
+        if cur.next.val == val:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next.next
+
+    if ret.val == val:
+        ret = ret.next
+
+    return ret
