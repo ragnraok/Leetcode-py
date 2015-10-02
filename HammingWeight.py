c5a43f5042a562c1b74d771d2d27ebc31f9840be
ragnraok <okone1288@gmail.com>
2015-10-02 23:39:53 +0800

    add anoter solution for HammingWeight

--- HammingWeight.py
+++ HammingWeight.py
@@ -1,3 +1,8 @@
 def hammingWeight(n):
-    str_repr = bin(n)[2:]
-    return len(filter(lambda x: x == '1', str_repr))
+    #str_repr = bin(n)[2:]
+    #return len(filter(lambda x: x == '1', str_repr))
+    ret = 0
+    for i in xrange(32):
+        ret += (n >> i) & 1
+
+    return ret
